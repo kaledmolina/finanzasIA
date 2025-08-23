023ca0d94d90bff7e2a128ed687ff44dfd4b296e
kaledmolynft <kaledmolynft@gmail.com>
2025-08-23 22:08:08 +0000

    first commit

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::group(['namespace' => 'App\Http\Controllers\API'], function () {
+    // --------------- Register and Login ----------------//
+    Route::post('register', 'AuthenticationController@register')->name('register');
+    Route::post('login', 'AuthenticationController@login')->name('login');
+
+    Route::get('/health', function () {
+        return response()->json(['status' => 'ok']);
+    });
+    
+    // ------------------ Get Data ----------------------//
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('get-user', 'AuthenticationController@userInfo')->name('get-user');
+        Route::post('logout', 'AuthenticationController@logOut')->name('logout');
+    });
+});
